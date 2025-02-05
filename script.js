6a498ad29f8413f39d78aeca07890bdb430c6ad3
Arbi.PNG <156532958+ejaaar@users.noreply.github.com>
2025-02-05 14:50:43 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+/*
+    NILAI 91-100 GRADE A
+    NILAI 81-90 GRADE B 
+    NILAI 71-80 GRADE B-
+    NILAI 61-70 GRADE C
+    NILAI 0-60 GRADE D
+*/
+
+
+alert('masukan nilai siswa')
+let inputNilai = prompt("input nilai")
+
+
+
+if(inputNilai >= 101){
+    alert('ngawurrr')
+}else if(inputNilai >= 91){
+    alert('A')
+}else if(inputNilai >= 81){
+    alert('B')
+}else if(inputNilai >= 71){
+    alert('B-')
+}else if(inputNilai >=61){
+    alert('C')
+}else if(inputNilai >=0){
+    alert('D')
+}
+
+
+// alert = untuk menampilkan pemberitahuan
+// prompt = untuk menampilkan pemberitahuan dan dapat di isi
+//let inputNilai = Prompt("input nilai")
+// let = tipe datanya
+// inputNilai = variabelnya
+
+//if = untuk pengkondisian
+//else = untuk pengkondisian di kondisi lain
+
+
+/* 
+else if(inputNilai >= 91){
+    alert('A')
+
+= membuat variabel (inputNilai) dengan nilai lebih besar sama dengan 91 berpredikat 'A'
+}
+*/
+
+
